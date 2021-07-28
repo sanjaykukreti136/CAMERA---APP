0bc4850d74acb4e61a536bcabaa5cae84fbdb7ec
Sanjay <sanjaykukreti900@gmail.com>
2021-07-28 18:40:42 +0530

    zoom feature updates

--- script.js
+++ script.js
@@ -10,9 +10,31 @@ let body = document.querySelector('body');
 let isRec = false;
 let capbtn = document.querySelector("#capture");
 let currsrc;
-
+let zoomin = document.querySelector("#zoomin");
+let zoomout = document.querySelector("#zoomout");
+let currzoomlevel = 1;
 let color = "";
 let allfilters = document.querySelectorAll('.filter');
+
+
+//// zooom in button function
+zoomin.addEventListener('click', function () {
+    /// increase zoom level 
+    currzoomlevel = currzoomlevel + 0.1;
+    if (currzoomlevel > 3) currzoomlevel = 3;
+    console.log(currzoomlevel);
+    /// set current zoom style to videoplayer 
+    videoplayer.style.transform = `scale(${currzoomlevel})`;
+})
+
+zoomout.addEventListener('click', function () {
+    currzoomlevel = currzoomlevel - 0.1;
+    if (currzoomlevel < 1) currzoomlevel = 1;
+    videoplayer.style.transform = `scale(${currzoomlevel})`;
+})
+
+
+
 for (let i = 0; i < allfilters.length; i++) {
     allfilters[i].addEventListener('click', function (e) {
 
@@ -31,6 +53,7 @@ for (let i = 0; i < allfilters.length; i++) {
 
 
 recbtn.addEventListener("click", function () {
+    videoplayer.style.transform = `scale(${1})`;
     let span = recbtn.querySelector('span');
     if (isRec) {
         span.classList.remove('animations')
@@ -90,11 +113,19 @@ capbtn.addEventListener('click', function () {   //// capture button par click h
     let tool = can.getContext('2d');
 
 
+    ////  canvass ko stretch karne ke liye center position me fixed
+    tool.translate(can.width / 2, can.height / 2);
+    /// canvas ko stretch
+    tool.scale(currzoomlevel, currzoomlevel);
+    //// canvas back to normal position 
+    tool.translate(-can.width / 2, -can.height / 2);
     tool.drawImage(videoplayer, 0, 0);
     if (color != "") {
         tool.fillStyle = color;
         tool.fillRect(0, 0, can.width, can.height)
     }
+
+    //// function for downloading image 
     downloadkaro(can.toDataURL());
 
 })
